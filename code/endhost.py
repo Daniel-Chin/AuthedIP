c00b24d539b74ee1c21587c77330effc5196c468
Daniel Chin <daniel.chin@nyu.edu>
2022-03-31 11:37:38 -0400

    endhost

--- code/endhost.py
+++ code/endhost.py
@@ -1,3 +1,4 @@
+from socket import socket
 from shared import *
 from packet import *
 from user import User
@@ -6,17 +7,16 @@ class Endhost:
     def __init__(self) -> None:
         self.ip_addr = ...
 
+        # represents physical link
+        self.sock : socket = ...
+
     def send(self, user : User, dest_addr, data):
         packet = AuthedIpPacket()
         packet.source_addr = self.ip_addr
-        packet.dest_addr = dest_addr
+        packet.  dest_addr = dest_addr
         packet.content = data
 
-        packet.rsa_public_key_id = user.rsa_public_key
-        packet.signHash(user.rsa_private_key)
+        packet.rsa_public_key_id = user.pubKeyId()
+        packet.sign(user.priKey)
         
-        self.ship(packet.asIPPacket())
-
-    def ship(self, packet : IPPacket):
-        # Ships a packet into the net. 
-        return NotImplemented('ship', packet) / 0
+        packet.asIPPacket().send(self.sock)
